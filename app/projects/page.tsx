9c5338cfa5355ae55209ed9dee2f002d5e9bee29
Kevin Chau <kchau57@uwo.ca>
2025-05-16 10:51:45 -0400

    fix: add content to projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProjectsPage() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-50 py-12">
+        <h1>Explore our past projects.</h1>
+    </section>
+  );
+}
